2eb14765d3317d255b98538edd0566f97e4e2da9
Jack <jackychang640@gmail.com>
2024-09-27 15:01:14 -0600

    completed the lab

--- lab3_part3/lab3_part3.cpp
+++ lab3_part3/lab3_part3.cpp
@@ -4,7 +4,21 @@
 #include <iostream>
 using namespace std;
 
+int a = 5;
+typedef int* VEC;
+typedef VEC* MATRIX;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	//declaring & defining pointers to the int a & VEC v
+	VEC v = &a;
+	MATRIX m = &v;
+
+	cout << "value of a = " << a << "\n";
+	//just printing the value of v will show the memory address,
+	//not the value of a. so, we have to use *.
+	cout << "value of a using VEC = " << *v << "\n";
+	//a single * is not enough for m, since it is already pointing
+	//to a pointer. so, we have to use ** to get the value of a.
+	cout << "value of a using MATRIX = " << **m;
 }
